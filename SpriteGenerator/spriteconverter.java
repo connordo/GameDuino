1c18447a24394f1e435a035421ab40b0632eb291
Connor <connorolsen72@gmail.com>
2018-04-21 21:29:36 -0600

    ppm file changes I believe

---
+++ SpriteGenerator/spriteconverter.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class spriteconverter(){
+public static void main(String[] args){
+    System.out.println("Testing the java interface");
+}

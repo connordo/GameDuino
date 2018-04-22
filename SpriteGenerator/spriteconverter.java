03adbc1ad2f606c4a906dd2d53ad5397b58557f0
Connor <connordo@github.com>
2018-04-21 22:59:55 -0600

    started the java program for converting files to sprites

--- SpriteGenerator/spriteconverter.java
+++ SpriteGenerator/spriteconverter.java
@@ -3,7 +3,18 @@ import java.util.*;
 import java.lang.*;
 
 
-public class spriteconverter(){
-public static void main(String[] args){
-    System.out.println("Testing the java interface");
+public class spriteconverter{
+	public static void main(String[] args){
+		if (args.length>1)
+		return;
+		System.out.println("Converting the file..\n\n\n\n");
+		try{
+			File imagefilepath = new File(args[0]);
+			Scanner scanner = new Scanner(imagefilepath);
+			while(scanner.hasNext()){
+				scanner.next();
+			}
+		}
+		catch(IOException ex){System.err.println(ex);}
+	}
 }

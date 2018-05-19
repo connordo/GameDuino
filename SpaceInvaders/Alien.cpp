98e8ae08fd9e6f9833d18f1cd9a07d9cbd9df3fc
Connor <connorolsen72@gmail.com>
2018-05-18 21:03:03 -0600

    updated the alien animation to no longer require silly little for loops

--- SpaceInvaders/Alien.cpp
+++ SpaceInvaders/Alien.cpp
@@ -2,10 +2,18 @@
 #include "Arduino.h"
 #define RANDOMFIRELOW 100
 #define RANDOMFIREHIGH 500
-unsigned char Alien::sprite_bmp[]=
+
+const unsigned char PROGMEM Alien::alien_up_bmp[]=
 {
   0b00100000, 0b10000000, 0b00010001, 0b00000000, 0b00111111, 0b10000000, 0b01101110, 0b11000000, 0b11111111, 0b11100000, 0b10111111, 0b10100000, 0b10100000, 0b10100000, 0b00011011, 0b00000000,
 };
+
+const unsigned char PROGMEM Alien::alien_down_bmp[]=
+{
+  0b00100000, 0b10000000, 0b10010001, 0b00100000, 0b10111111, 0b10100000, 0b11101110, 0b11100000, 0b11111111, 0b11100000, 0b01111111, 0b11000000, 0b00100000, 0b10000000, 0b01000000, 0b01000000,
+};
+
+
 
 Alien::Alien(Adafruit_SSD1306 *display, int init_x_pos, int init_y_pos)
 :Entity(display, init_x_pos, init_y_pos){
@@ -20,62 +28,55 @@ Alien::Alien(Adafruit_SSD1306 *display, int init_x_pos, int init_y_pos)
   fireCounter = 0;
   FireMax = (int)random(RANDOMFIRELOW, RANDOMFIREHIGH);
   currentSprite = 1;
-  for(int i = 0; i < 16; i++){
-    sprite_bmp[i] = alien1[i];
-  }
 }
 
 void Alien::move(int d){
-  display->drawBitmap(x_pos, y_pos, sprite_bmp, width, height, BLACK);
+  display->drawBitmap(x_pos, y_pos, getSpriteBmp(), width, height, BLACK);
   switch(d){
     case 0:
-    display->drawBitmap(x_pos, --y_pos, sprite_bmp, width, height, WHITE);
+    display->drawBitmap(x_pos, --y_pos, getSpriteBmp(), width, height, WHITE);
     break;
     case 1:
-    display->drawBitmap(x_pos, ++y_pos, sprite_bmp, width, height, WHITE);
+    display->drawBitmap(x_pos, ++y_pos, getSpriteBmp(), width, height, WHITE);
     break;
     case 2:
-    display->drawBitmap(--x_pos, y_pos, sprite_bmp, width, height, WHITE);
+    display->drawBitmap(--x_pos, y_pos, getSpriteBmp(), width, height, WHITE);
     break;
     case 3:
-    display->drawBitmap(++x_pos, y_pos, sprite_bmp, width, height, WHITE);
+    display->drawBitmap(++x_pos, y_pos, getSpriteBmp(), width, height, WHITE);
     break;
   }
 }
 
 void Alien::explode()
 {
-	display->drawBitmap(x_pos, y_pos, sprite_bmp, width, height, BLACK);
-
+  display->drawBitmap(x_pos, y_pos, getSpriteBmp(), width, height, BLACK);
 }
 
 void Alien::animate(){
   if(animationCounter++ >= animationSpeed){
+    Serial.begin(9600);
     display->fillRect(x_pos, y_pos, width, height, BLACK);
-    switch((currentSprite == 1 ? currentSprite = 2 : currentSprite = 1)){
-      case 1:
-      for(int i = 0; i < 16; i++){
-        sprite_bmp[i] = alien1[i];
-      }
-      break;
-      case 2:
-      for(int i = 0; i < 16; i++){
-        sprite_bmp[i] = alien2[i];
-      }
-      break;
-      default:
-      break;
-    }
-    display->drawBitmap(x_pos, y_pos, sprite_bmp, width, height, WHITE);
+    currentSprite == 1 ? currentSprite = 2 : currentSprite = 1;
+    Serial.println(currentSprite);
+    display->drawBitmap(x_pos, y_pos, getSpriteBmp(), width, height, WHITE);
     animationCounter = 0;
   }
 }
 
-
 void Alien::draw(){
-  display->drawBitmap(x_pos, y_pos, sprite_bmp, width, height, WHITE);
+  display->drawBitmap(x_pos, y_pos, getSpriteBmp(), width, height, WHITE);
 }
 
 bool Alien::isAlive(){
   return alive;
+}
+
+const unsigned char* Alien::getSpriteBmp(){
+  if(currentSprite == 1){
+    return alien_up_bmp;
+  }
+  else{
+    return alien_down_bmp;
+  }
 }
--- SpaceInvaders/Alien.h
+++ SpaceInvaders/Alien.h
@@ -7,19 +7,10 @@ private:
   bool alive;
 
 public:
-  static unsigned char sprite_bmp[];
-  uint16_t alien1[16]=
-  {
-    0b00100000, 0b10000000, 0b00010001, 0b00000000, 0b00111111, 0b10000000, 0b01101110, 0b11000000, 0b11111111, 0b11100000, 0b10111111, 0b10100000, 0b10100000, 0b10100000, 0b00011011, 0b00000000,
-  };
-
-  uint16_t alien2[16]=
-  {
-    0b00100000, 0b10000000, 0b10010001, 0b00100000, 0b10111111, 0b10100000, 0b11101110, 0b11100000, 0b11111111, 0b11100000, 0b01111111, 0b11000000, 0b00100000, 0b10000000, 0b01000000, 0b01000000,
-  };
+  static const unsigned char PROGMEM alien_up_bmp[];
+  static const unsigned char PROGMEM alien_down_bmp[];
 
   int currentSprite;
-  //Bullet *alienbullet;
   int fireCounter;
   int FireMax;
 
@@ -27,6 +18,8 @@ public:
   void draw();
   void move(int d);
   void explode();
+  const unsigned char* getSpriteBmp();
+
 
   Alien(Adafruit_SSD1306 *display, int init_x_pos, int init_y_pos);
   bool isAlive();
